b86a35b3b5c2cde3766ea18e67062e6508f6aeee
Moiz-16 <MoizSaleem903@gmail.com>
2024-08-20 01:04:18 +0100

    temporary fix for homescreen layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" className='!scroll-smooth'>
       <body className='${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-40 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90'>
         <div className=' bg-[#1e96fc] absolute top[-6rem] -z-10 right-[11rem] h-[29.25rem]
-         w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+         w-[31.25rem] rounded-full blur-[9rem] sm:w-[68.75rem]'></div>
         <div className='  bg-[#072ac8] absolute top[-1rem] -z-10 left-[-35rem] h-[26.25rem]
          w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem]
          lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
