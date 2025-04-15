6833ba61b7025cf3da525082270076828d7b8f98
Moiz-16 <MoizSaleem903@gmail.com>
2025-04-15 01:50:06 +0100

    replaced rect-type-animation with react-typed

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,80 +1,93 @@
 "use client";
 
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import React, { useEffect } from 'react'
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React, { useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
-import { BsArrowRight, BsGithub, BsLinkedin } from 'react-icons/bs'
-import { FaGithub} from 'react-icons/fa6';
-import { useInView} from 'react-intersection-observer'
-import { useActiveSectionContext } from '@/context/active-section-context'
-import { useSectionInView } from '@/lib/hooks';
-import { SocialIcon } from 'react-social-icons'
-import { useTheme } from '@/context/theme-context';
-
-
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa6";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useSectionInView } from "@/lib/hooks";
+import { SocialIcon } from "react-social-icons";
+import { useTheme } from "@/context/theme-context";
+import { ReactTyped } from "react-typed";
 
 export default function Intro() {
-    const { ref } = useSectionInView("Home", 0.5)
-    const {setActiveSection, setTimeOfLastClick} = useActiveSectionContext();
-    const {theme } = useTheme();
-
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const { theme } = useTheme();
 
   return (
-    <section ref={ref} id='home' className='scroll-mt-[100rem] my-28'>
-        <motion.div className='flex flex-col items-center justify-center'
-        initial={{opacity: 0, scale: 0 }}
-        animate={{opacity: 1, scale: 1 }}
-        transition={{type: "tween", delay: 0.1, duration: 0.3}}
-        >
+    <section ref={ref} id="home" className="scroll-mt-[100rem] my-28">
+      <motion.div
+        className="flex flex-col items-center justify-center"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ type: "tween", delay: 0.1, duration: 0.3 }}
+      >
         <h1 className="md:text-6xl sm:text-5xl text-4xl font-bold text-black">
-            Hi! I am Moiz Saleem
+          Hi! I am Moiz Saleem
         </h1>
         <h1 className="md:text-9xl sm:text-7xl text-7xl font-bold text-black md:py-6 py-3">
-            <TypeAnimation
-              
-              sequence={["Student", 2000, "Developer", 2000]}
-              wrapper="span"
-              speed={65}
-              repeat={Infinity}
-            />
+          <ReactTyped
+            strings={["Student", "Developer"]}
+            typeSpeed={75}
+            backSpeed={50}
+            backDelay={1000}
+            loop
+          />
         </h1>
-        
-        </motion.div>
-        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-5 
-        px-4 py-10 text-lg font-medium' 
+      </motion.div>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-5 
+        px-4 py-10 text-lg font-medium"
         initial={{ y: 100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{delay: 0.2}}
-        >
-            <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex 
+        transition={{ delay: 0.2 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex 
             items-center  gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 
-            hover:bg-gray-950 active:scale-105 transition'
-            onClick={() => {
-                setActiveSection("Contact")
-                setTimeOfLastClick(Date.now());
-            }}
-            >
-                Contact me here <BsArrowRight className='group-hover:opacity-60 group-hover:translate-x-2 transition '/>
-
-            </Link>
-            <div className='bg-white bg-opacity-90 flex items-center gap-2 rounded-full
+            hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here{" "}
+          <BsArrowRight className="group-hover:opacity-60 group-hover:translate-x-2 transition " />
+        </Link>
+        <div
+          className="bg-white bg-opacity-90 flex items-center gap-2 rounded-full
             focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border
-             dark:bg-gray-950 dark:bg-opacity-75 border-black/10 '
-            >
-                <div><SocialIcon  bgColor="transparent" fgColor="currentColor"
-                style={{ color: theme === "light" ? "black" : "white", }}   url="https://www.linkedin.com/in/moiz-saleem/" /></div>
-                
-            </div>
-            <div className='bg-white bg-opacity-90 flex items-center gap-2 rounded-full
+             dark:bg-gray-950 dark:bg-opacity-75 border-black/10 "
+        >
+          <div>
+            <SocialIcon
+              bgColor="transparent"
+              fgColor="currentColor"
+              style={{ color: theme === "light" ? "black" : "white" }}
+              url="https://www.linkedin.com/in/moiz-saleem/"
+            />
+          </div>
+        </div>
+        <div
+          className="bg-white bg-opacity-90 flex items-center gap-2 rounded-full
             focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border
-             dark:bg-gray-950 border-black/10 dark:bg-opacity-75'
-            >
-                <div><SocialIcon  bgColor="transparent" fgColor="currentColor"
-                style={{ color: theme === "light" ? "black" : "white", }}   url="https://github.com/Moiz-16" /></div>
-                
-            </div>
-{/* 
+             dark:bg-gray-950 border-black/10 dark:bg-opacity-75"
+        >
+          <div>
+            <SocialIcon
+              bgColor="transparent"
+              fgColor="currentColor"
+              style={{ color: theme === "light" ? "black" : "white" }}
+              url="https://github.com/Moiz-16"
+            />
+          </div>
+        </div>
+        {/* 
             <a>
                 <SocialIcon url="https://linkedin.com/in/" />
 
@@ -95,10 +108,7 @@ export default function Intro() {
                 className=" sm:h-12 h-10"
               />
             </a> */}
-
-
-        </motion.div>
-
+      </motion.div>
     </section>
-  )
+  );
 }
